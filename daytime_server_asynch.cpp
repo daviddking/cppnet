593a5cd34b34e9ca7bd2d6ead4a084f91a182afd
David King <daviddking@gmail.com>
2017-05-31 15:24:50 -0500

    wip

--- daytime_server_asynch.cpp
+++ daytime_server_asynch.cpp
@@ -10,17 +10,56 @@ using namespace std;
 using namespace std::chrono;
 using namespace std::experimental::net;
 using namespace std::experimental::net::ip;
+using namespace std::placeholders;
 
 int const DAYTIME_PORT = 8013;
 
+static void glob_write_handler(const std::error_code& error, std::size_t bytes_transferred) {
+
+}
+
+
+class daytime_tcp_connection {
+public:
+    daytime_tcp_connection(tcp::socket&& client_socket)  : client_socket{std::move(client_socket)}
+    {
+    }
+
+    void write_handler(error_code& error, size_t bytes_transferred) {
+
+    }
+
+    void start() {
+        string message = current_date_and_time();
+        client_socket.async_write_some(
+                buffer(message),
+                &glob_write_handler);
+//                bind(&daytime_tcp_connection::write_handler, this, _1, _2));
+    }
+
+
+private:
+    tcp::socket client_socket;
+};
+
+
 class daytime_tcp_server {
 public:
-    daytime_tcp_server(shared_ptr<io_context> io, int listen_port) :
-            io{io}, acceptor{tcp::v4(), listen_port}
+    daytime_tcp_server(shared_ptr<io_context> io, unsigned short listen_port) :
+            io{io}, acceptor{*io, tcp::endpoint{tcp::v4(), listen_port}}
     {}
 
     void start_accept() {
-        
+        acceptor.async_accept(bind(&daytime_tcp_server::accept_handler, this, _1, _2));
+    }
+
+    void accept_handler(const error_code& error, tcp::socket client_socket) {
+        if (!error) {
+            auto client_connection =
+                    make_shared<daytime_tcp_connection>(client_socket);
+            client_connection->start();
+        }
+        start_accept();
     }
 
 private:
@@ -29,8 +68,9 @@ private:
 };
 
 int main() {
-    io_context io;
-    tcp_server tcpServer{io, DAYTIME_PORT};
-    io.run();
+    auto io = make_shared<io_context>();
+    daytime_tcp_server tcpServer{io, DAYTIME_PORT};
+    io->run();
 }
+
 #pragma clang diagnostic pop
